e33c89a673bdd582b252781d606fa4137aa8a8df
pillapavan <pillapavan90909@gmail.com>
2025-04-06 22:00:05 +0530

    update peakElement with duplicates

--- src/com/java/binarySearch/FindPeakIndex.java
+++ src/com/java/binarySearch/FindPeakIndex.java
@@ -2,8 +2,8 @@ package com.java.binarySearch;
 //https://leetcode.com/problems/find-peak-element/
 public class FindPeakIndex {
     public static void main(String[] args) {
-        int[] arr={1,2,3,1};
-        System.out.println(findPeakElement(arr));
+        int[] arr={1,3,6,6,6,6,7,4,3,2,1};
+        System.out.println(findPeakElementWithDuplicates(arr));
     }
     static int findPeakElement(int[] arr){
         int start=0;
@@ -17,6 +17,27 @@ public class FindPeakIndex {
             }
         }
      return start;
+    }
+
+    static int findPeakElementWithDuplicates(int[] arr){
+        int start=0;
+        int end= arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            int pMid=mid;
+            while(pMid<end-1 && arr[pMid]==arr[pMid+1]){
+                pMid++;
+            }
+            if(start!=end-1 && pMid==end-1){
+                end=mid;
+            }
+            else if(arr[pMid]>arr[pMid+1]){
+                end=pMid;
+            }else{
+                start=mid+1;
+            }
+        }
+       return arr[start];
     }
 
 }
